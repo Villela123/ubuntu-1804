e03096e73b8c03e574b5837411697d9eababbfbb
Villela123 <43385609+Villela123@users.noreply.github.com>
2019-01-17 16:33:46 -0200

    Update atualizar.sh

--- atualizar.sh
+++ atualizar.sh
@@ -6,27 +6,37 @@ echo
     sudo apt update
 echo 
 echo Listas atualizadas
+sleep 5
+clear
 
 echo Atualizando os Softwares
 echo 
     sudo apt -y upgrade
 echo 
 echo Softwares atualizados
+sleep 5
+clear
 
 echo Atualizando o Kernel
 echo 
     sudo apt -y dist-upgrade
 echo 
 echo Kernel atualizado
+sleep 5
+clear
 
 echo Removendo os Softwares
 echo 
     sudo apt -y autoremove
 echo 
 echo Softwares removidos
+sleep 5
+clear
 
 echo Limpando o Cache
 echo 
     sudo apt -y autoclean
 echo 
 echo Cache limpo
+sleep 5
+clear
